b10229c226a76299d61238c94b3764c3ac078354
bcwein <bcwein343@gmail.com>
2019-09-21 12:54:49 +0200

    La inn onedrive

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-sudo pacman -S geogebra ipython python-numpy python-scipy python-matplotlib clang gcc code android-file-transfer gnome-calendar discord deja-dup virtualbox pulseeffects remmina vim
-pamac build google-chrome spotify mailspring anaconda cisco-anyconnect nordvpn-bin
+sudo pacman -S geogebra ipython python-numpy python-scipy python-matplotlib clang gcc code android-file-transfer \
+gnome-calendar discord deja-dup virtualbox pulseeffects remmina vim curl sqlite dlang
+pamac build google-chrome spotify mailspring anaconda cisco-anyconnect nordvpn-bin onedrive
 pamac upgrade
 sudo pacman -Syyuu
 sudo pacman -Scc
