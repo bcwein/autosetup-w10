ba25d570249ddbc6e5a23988b8d756b85358d735
bcwein <bcwein343@gmail.com>
2019-10-07 10:44:17 +0200

    PS3 emulator

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo pacman -S geogebra ipython python-numpy python-scipy python-matplotlib clang gcc code android-file-transfer \
 gnome-calendar discord deja-dup virtualbox pulseeffects remmina vim curl sqlite dlang
-pamac build google-chrome spotify mailspring anaconda cisco-anyconnect nordvpn-bin onedrive
+pamac build google-chrome spotify mailspring anaconda cisco-anyconnect nordvpn-bin onedrive rpcs3
 pamac upgrade
 sudo pacman -Syyuu
 sudo pacman -Scc
