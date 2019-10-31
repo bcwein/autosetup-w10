7002af0451657a7a6131328533b33a3d1803400b
bcwein <bcwein343@gmail.com>
2019-10-31 22:53:01 +0100

    Ubuntu script

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-sudo pacman -S geogebra ipython python-numpy python-scipy python-matplotlib clang gcc code android-file-transfer \
-gnome-calendar discord deja-dup virtualbox pulseeffects remmina vim curl sqlite dlang vlc
-pamac build google-chrome spotify mailspring anaconda cisco-anyconnect nordvpn-bin onedrive rpcs3 pdk
-pamac upgrade
-sudo pacman -Syyuu
-sudo pacman -Scc
+sudo apt-get install geogebra ipython python-numpy python-scipy python-matplotlib clang gcc \
+remmina vim vlc
+
+sudo apt install curl
+
+sudo snap install spotify
+sudo snap install discord --classic
+sudo snap install mailspring
+
+cd /tmp
+curl -O https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
+bash Anaconda3-2019.10-Linux-x86_64.sh
+source ~/.bashrc
+
