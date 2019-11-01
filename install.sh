c36816faba2430f0954054e03c8a8a8e485e2023
bcwein <bcwein343@gmail.com>
2019-11-01 13:06:00 +0100

    La inn installasjon av onedrive

--- install.sh
+++ install.sh
@@ -7,10 +7,20 @@ sudo apt install curl snapd
 sudo snap install spotify
 sudo snap install discord --classic
 sudo snap install mailspring
-sudo snap install deja-dup --classic
 
 cd /tmp
 curl -O https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
 bash Anaconda3-2019.10-Linux-x86_64.sh
 source ~/.bashrc
 
+sudo apt install libcurl4-openssl-dev git
+sudo apt install libsqlite3-dev
+sudo snap install --classic dmd && sudo snap install --classic dub
+
+git clone https://github.com/abraunegg/onedrive.git
+cd onedrive
+./configure
+make
+sudo make install
+
+onedrive
