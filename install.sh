0519ddc8fe19335bc023ee325a57984edc98bb5b
bcwein <bcwein343@gmail.com>
2019-11-01 08:44:40 +0100

    La inn deja-dup og snapd

--- install.sh
+++ install.sh
@@ -2,11 +2,12 @@
 sudo apt-get install geogebra ipython python-numpy python-scipy python-matplotlib clang gcc \
 remmina vim vlc
 
-sudo apt install curl
+sudo apt install curl snapd
 
 sudo snap install spotify
 sudo snap install discord --classic
 sudo snap install mailspring
+sudo snap install deja-dup --classic
 
 cd /tmp
 curl -O https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
