1ad5946394b83bb6ff196001ad0fdd3a068a689c
bcwein <bcwein343@gmail.com>
2019-11-11 10:51:22 +0100

    La inn installasjon av cisco anyconnect

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ curl -O https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
 bash Anaconda3-2019.10-Linux-x86_64.sh
 source ~/.bashrc
 
-sudo apt install libcurl4-openssl-dev git
+sudo apt install libcurl4-openssl-dev git 
 sudo apt install libsqlite3-dev
 sudo snap install --classic dmd && sudo snap install --classic dub
 
@@ -24,3 +24,12 @@ make
 sudo make install
 
 onedrive
+
+apt install -y lib32z1 lib32ncurses5
+apt install network-manager-openconnect
+systemctl daemon-reload
+
+cd ~/Downloads
+tar xf vpn_4603049_linux.gz
+cd ./anyconnect-linux64-4.6.03049/vpn/
+sudo ./vpn_install.sh
