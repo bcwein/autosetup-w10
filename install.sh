b906a6146fde6d6aa534d141c7f2320ab48fb1c1
bcwein <bcwein343@gmail.com>
2020-03-14 23:00:41 +0100

    Update installscript for ubuntu

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 sudo apt-get install geogebra ipython python-numpy python-scipy python-matplotlib clang gcc \
-remmina vim vlc
+remmina vim vlc gnome-maps gnome-tweaks git python3-gi python3-xlib powertop
 
 sudo apt install curl snapd
 
 sudo snap install spotify
 sudo snap install discord --classic
 sudo snap install mailspring
+sudo snap install cawbird
 
 cd /tmp
 curl -O https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
