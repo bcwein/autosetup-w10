eb4cdbc833e3c0f94efd3f8a12df52ab6d519c9e
bcwein <bcwein343@gmail.com>
2020-12-26 15:31:38 +0100

    Help-script added for exporting config

---
+++ Export-Chocolatey.ps1
@@ -0,0 +1,11 @@
+# Code from https://gist.github.com/alimbada/449ddf65b4ef9752eff3
+
+# Put this in Export-Chocolatey.ps1 file and run it:
+# .\Export-Chocolatey.ps1 > packages.config
+# You can install the packages using
+# choco install packages.config -y
+
+Write-Output "<?xml version=`"1.0`" encoding=`"utf-8`"?>"
+Write-Output "<packages>"
+choco list -lo -r -y | ForEach-Object { "   <package id=`"$($_.SubString(0, $_.IndexOf("|")))`" version=`"$($_.SubString($_.IndexOf("|") + 1))`" />" }
+Write-Output "</packages>"
