f3e10a57d0a6ffb11515e16c00671c79f566520e
bcwein <bcwein343@gmail.com>
2020-12-26 15:30:15 +0100

    Install script

---
+++ autosetup-w10.ps1
@@ -0,0 +1,13 @@
+﻿# Install chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+$config = ".\env.config"
+
+if (Test-Path $config) {
+    choco install env.config
+    choco upgrade all
+    # Export upgraded packages to env.config
+    .\Export-Chocolatey.ps1 > env.config
+} else {
+    Write-Output("Config file Missing")
+}
